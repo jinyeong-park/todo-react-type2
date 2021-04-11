c9760b6a4693dd6ccf6953ce80dc848276cce04f
Jinyeong Park <jinyeongpark1205@gmail.com>
2021-04-10 18:27:19 -0700

    Add useState to save task in app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import '../App.css';
 
 function App() {
+  // create useState to save task
+  const [tast, setTask] = useState<string>("")
+
   return (
     <div className="App">
       {/* separate two sections */}
       <div className='header'>
         <div className='inputContainer'>
-          <input type='number' placeholder='Deadline (in Days)...'></input>
           <input type='text' placeholder='Task..'></input>
+          <input type='number' placeholder='Deadline (in Days)...'></input>
         </div>
         <button className='button'>Add Task</button>
       </div>
